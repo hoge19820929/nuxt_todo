60afb5cd8003ab66ebbc0388d8df384661022789
unknown <hoge@example.com>
2020-05-25 14:58:23 +0900

    fix for GitHub Pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,5 +49,13 @@ export default {
     */
     extend (config, ctx) {
     }
-  }
+  },
+
+  ...routerBase
 }
+
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/<repository-name>/'
+  }
+} : {}
